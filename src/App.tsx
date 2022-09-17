8b3928f26b1041264066c35ed0a8d206658dccf8
shunakid <shunakid@icloud.com>
2022-09-17 22:35:02 +0900

    useState導入、Todoリスト全体と個別のState定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useState } from "react";
 
-type User = {
+type Todos = {
   id: number;
   title: string;
 };
 
 const App = () => {
-  const todos: Array<User> = [
+  //Todoリスト全体
+  const [todos, setTodos] = useState<Todos[]>([
     { id: 1, title: "todo1" },
     { id: 2, title: "todo2" },
     { id: 3, title: "todo3" },
-  ];
+  ]);
+
+  //個別のTodo
+  const [todoTitle, setTodoTitle] = useState("");
 
   return (
     <>
