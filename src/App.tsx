088a1738fdd89840e174e710097bdbc79390a71b
shunakid <shunakid@icloud.com>
2022-09-17 21:52:42 +0900

    ダミーのリスト作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import ReactDOM from "react-dom";
 import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const todos = [
+    { id: 1, title: "todo1" },
+    { id: 2, title: "todo2" },
+    { id: 3, title: "todo3" },
+  ];
+
+  return (
+    <>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default App;
